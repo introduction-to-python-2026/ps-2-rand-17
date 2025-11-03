cfc3c40d73216796e96abbe1cbb9488f595f90f0
rand-17 <mohamedrand3@gmail.com>
2025-11-03 14:57:20 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,19 +1,23 @@
 def find_max_number(num1, num2, num3):
-     if num1 > num2 :
+    if num1 > num2 :
       if num1 > num3 :
-       return num1 
+        return num1 
       else :
         return num3
-    else :
+    
+     else :
       if num2 > num3 :
         return num2
       else : 
         return num3
 
+
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    mean = (num1 + num2 + num3) / 3
+    return mean
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
+    sigma = (((num1-mean)**2+(num2-mean)**2+(num3-mean)**2)/3)**0.5
     pass  # Replace 'pass' with code
 
